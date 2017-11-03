b6c47f4681cddc93e405de703e05bbf338754489
xingfeiy <yxfly@hotmail.com>
2017-11-02 22:57:06 -0700

    add more

---
+++ src/main/java/com/future/round2/Problem560.java
@@ -0,0 +1,30 @@
+package com.future.round2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by someone on 11/2/17.
+ */
+public class Problem560 {
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int diff = sum - k;
+            if(map.containsKey(diff)) {
+                count += map.get(diff);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subarraySum(new int[]{1, 1, 1}, 2));
+    }
+}
