693cdb7098dbbaebc04ef1e77e80f17bf47b81fd
xingfeiy <yxfly@hotmail.com>
2017-12-01 17:39:15 -0800

    add

---
+++ src/main/java/com/future/round2/Problem114.java
@@ -0,0 +1,63 @@
+package com.future.round2;
+
+import com.future.utils.TreeNode;
+
+/**
+ * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * Created by xingfeiy on 12/1/17.
+ */
+public class Problem114 {
+    /**
+     * Analyze:
+     * The straightforward way is flatten left and flatten right first and then move left to right, and then move the right
+     * to the tail of left.
+     * @param root
+     */
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode tmpRight = root.right;
+        root.right = root.left == null ? root.right : root.left;
+        root.left = null;
+        TreeNode tmp = root;
+        while (tmp.right != null) tmp = tmp.right;
+        tmp.right = tmpRight;
+    }
+
+    /**
+     * Analyze:
+     * In the above straightforward solution, the time complexity is O(nh) where h is the height of tree, because we have to
+     * find the tail of left subtree.
+     * In the above solution, the basic idea is flatten left and right side, that means we finished all node in left side
+     * and return back to process right side, when we start to process the right side, at this moment, we have the root of left subtree only.
+     * The root of left subtree is useless for us, we need the tail of subtree.
+     *
+     * But, we need the root of right subtree, we use that to connect with the tail node of left subtree.
+     * We traversal the right first, that means we will start from the right most node, we can use a variable to store it.
+     *
+     * for example
+     *    1
+     *   / \
+     *  2   3
+     *  After we flattened this tree, the tree looks like this:
+     *   1
+     *    \
+     *     2
+     *      \
+     *       3
+     *
+     * @param root
+     */
+    private TreeNode preNode = null;
+    public void flatten2(TreeNode root) {
+        if(root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = preNode;
+        root.left = null;
+        preNode = root;
+    }
+
+}
---
+++ src/main/java/com/future/round2/Problem124.java
@@ -0,0 +1,52 @@
+package com.future.round2;
+
+import com.future.foundation.java.MapLearning;
+import com.future.utils.TreeNode;
+
+/**
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
+ * Given a binary tree, find the maximum path sum.
+
+ For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the
+ parent-child connections. The path must contain at least one node and does not need to go through the root.
+
+ For example:
+ Given the below binary tree,
+
+   1
+  / \
+ 2   3
+ Return 6.
+ *
+ * Created by xingfeiy on 12/1/17.
+ */
+public class Problem124 {
+    /**
+     * Analyze:
+     * 1. Sequence of nodes, 2. at least one node, 3. doesn't need to go through the root.
+     *
+     * So for any subtree, the maximum path sum could be:
+     *  - root self.
+     *  - the maximum path sum of left child.
+     *  - the maximum path sum of right child
+     *  - root self + maximum_left_child
+     *  - root self + maximum_right_child.
+     *  - root self + maximum_left_child + maximum_right_child.
+     * @param root
+     * @return
+     */
+
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return maxValue;
+    }
+
+    private int maxValue = Integer.MIN_VALUE;
+    private int helper(TreeNode node) {
+        if(node == null) return 0;
+        int left = helper(node.left);
+        int right = helper(node.right);
+        maxValue = Math.max(maxValue, Math.max(left, 0) + Math.max(right, 0) + node.val);
+        return Math.max(Math.max(left, 0), Math.max(right, 0)) + node.val;
+    }
+}
