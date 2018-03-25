2f746a697f4809b749844b2e518e4e2bec2064cb
xingfeiy <yxfly@hotmail.com>
2018-03-25 15:22:03 -0700

    挨打的

---
+++ src/main/java/com/future/round2/Problem669.java
@@ -0,0 +1,53 @@
+package com.future.round2;
+
+import com.future.utils.TreeNode;
+
+/**
+ * https://leetcode.com/problems/trim-a-binary-search-tree/description/
+
+ Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
+
+ Example 1:
+ Input:
+     1
+    / \
+   0   2
+
+ L = 1
+ R = 2
+
+ Output:
+   1
+    \
+     2
+ Example 2:
+ Input:
+     3
+    / \
+   0   4
+    \
+    2
+   /
+  1
+
+ L = 1
+ R = 3
+
+ Output:
+      3
+     /
+    2
+   /
+  1
+ * Created by xingfeiy on 3/25/18.
+ */
+public class Problem669 {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if(root == null) return null;
+        if(root.val < L) return trimBST(root.right, L, R);
+        if(root.val > R) return trimBST(root.left, L, R);
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+}
